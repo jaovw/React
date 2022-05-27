b1ec7793ff19735825823d7a50ead76939dd54d9
joao.victor <joao.victor@superfestval.com.br>
2022-05-27 15:39:51 -0300

    Formatando

--- fundamentos-react/src/components/basic/Fragmento.jsx
+++ fundamentos-react/src/components/basic/Fragmento.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 
-export default props => {
-    return (    //  FORMA RESUMIDA DE FRAGMENT <> </> | SEM POSSIBILIDADE DE ATRIBUTOS
-        <React.Fragment key='1'>
-            <h2>Fragmento</h2>
-            <p>Cuidado com esse erro!</p> 
-        </React.Fragment>
-    )
-}
+export default _ =>
+    //  FORMA RESUMIDA DE FRAGMENT <> </> | SEM POSSIBILIDADE DE ATRIBUTOS
+    <React.Fragment key='1'>
+        <h2>Fragmento</h2>
+        <p>Cuidado com esse erro!</p>
+    </React.Fragment>
