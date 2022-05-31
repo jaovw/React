f53013e51deb6c85fe9ec6f709062885ac5c23e0
joao.victor <joao.victor@superfestval.com.br>
2022-05-31 16:24:18 -0300

    Utilizando map para arrayObj

---
+++ fundamentos-react/src/data/alunos.js
@@ -0,0 +1,12 @@
+export default [
+    {id: 1, nome: 'Ana', nota: 9.9},
+    {id: 2, nome: 'Beatriz', nota: 7.6},
+    {id: 3, nome: 'Carlos', nota: 6.1},
+    {id: 4, nome: 'Daniel', nota: 8.4},
+    {id: 5, nome: 'Guilherme', nota: 5.6},
+    {id: 6, nome: 'Rebeca', nota: 0.9},
+    {id: 7, nome: 'Arthur', nota: 7.2},
+    {id: 8, nome: 'Pedro', nota: 8.3},
+    {id: 9, nome: 'Gustavo', nota: 4.4},
+    {id: 10, nome: 'Maria', nota: 10}
+]
