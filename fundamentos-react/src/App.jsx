f8d9d7e1c6dcd6ce61c285fb212d96574ba6c1a2
joao.victor <joao.victor@superfestval.com.br>
2022-05-31 11:10:43 -0300

    Novo card componente

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -7,12 +7,17 @@ import ComParametro from './components/basic/ComParametro'
 import NotasAlunos from './components/basic/NotasAlunos'
 import Fragmento from './components/basic/Fragmento'
 import Aleatorio from "./components/basic/Aleatorio"
+import Familia from "./components/basic/Familia";
 
 export default _ =>
     <div className="App">
         <h1>Fundamentos React</h1>
 
         <div className="Cards">
+            <Card titulo='#06 - Componentes com filhos' color='#adad00'>
+                <Familia sobrenome='Santos'/>
+
+            </Card>
 
             <Card titulo='#05 - Número Aleatório' color='#080'>
                 <Aleatorio
