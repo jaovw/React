7b541d673bfebeb73386b27c1002bd58716e88e6
joao.victor <joao.victor@superfestval.com.br>
2022-06-01 09:03:50 -0300

    Formando tabela com function dinamica

--- fundamentos-react/src/components/repeticao/TabelaProdutos.jsx
+++ fundamentos-react/src/components/repeticao/TabelaProdutos.jsx
@@ -1,27 +1,36 @@
+import './TabelaProdutos.css'
 import React from "react";
 import produtos from "../../data/produtos";
 
 export default () => {
 
-    const listaProdutos = produtos.map(produto => {
-        return <th>{produto.nome}</th>
-    })
-    const listaPreco = produtos.map(produto => {
-        return <th>{produto.preco}</th>
-    })
+   function getLinhas() {
+       return produtos.map( (produto, i) => {
+        return (
+            <tr key={produto.id} className={i % 2 == 0 ? 'Par' : 'Impar'}>
+                <td>{produto.id}</td>
+                <td>{produto.nome}</td>
+                <td>R$ {produto.preco}</td>
+            </tr>
+        )
+       })
+    }
 
     return (
-        <div>
+        <div className="TabelaProdutos">
             <table>
-                <td>
-                    {listaProdutos}
-                </td>
-                <td>
-                    {listaPreco}
-                </td>
-
+                <thead>
+                    <tr>
+                        <td>ID</td>
+                        <td>Nome</td>
+                        <td>Preço</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {getLinhas()}
+                </tbody>
             </table>
-
         </div>
     )
+
 }
