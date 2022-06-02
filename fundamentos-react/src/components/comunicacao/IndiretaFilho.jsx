25a6b34cd6790935ad68240b5f8abcb353f3b727
joao.victor <joao.victor@superfestval.com.br>
2022-06-02 15:02:57 -0300

    Utilizando webHooks para estado do componente

--- fundamentos-react/src/components/comunicacao/IndiretaFilho.jsx
+++ fundamentos-react/src/components/comunicacao/IndiretaFilho.jsx
@@ -3,10 +3,12 @@ import React from "react";
 
 export default props => {
 
+    const gerarIdade = () => parseInt(Math.random() * (20)) + 50
+    const geraBool = () => Math.random() > 0.5
     return (
         <div>
             <div>Filho</div>
-            <button onClick={_ => props.quandoClicar('João', 44, true)}>
+            <button onClick={_ => props.quandoClicar('João', gerarIdade(), geraBool())}>
                 Fornecer Informações
             </button>
         </div>
--- fundamentos-react/src/components/comunicacao/IndiretaPai.jsx
+++ fundamentos-react/src/components/comunicacao/IndiretaPai.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import IndiretaFilho from "./IndiretaFilho";
 
 export default props => {
 
+    const [nome, setNome] = useState('')
+    const [idade, setIdade] = useState(8)
+    const [nerd, setNerd] = useState(false)
+
     function info(nome, idade, nerd) {
-        console.log(nome, idade, nerd)
+        // console.log(nome, idade, nerd)
+
+        setNome(nome)
+        setIdade(idade)
+        setNerd(nerd)
+
     }
     return (
         <div>
-           Pai
+           <div>
+               <span>{nome} </span>
+               <span><strong>{idade}</strong></span>
+               <span> {nerd ? 'Verdadeiro' : 'Falso'}</span>
+           </div>
            <IndiretaFilho quandoClicar={info}/>
         </div>
     )
