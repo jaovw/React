9390412c0b2f9f4f5cee4ce341b0b0ed1fad8444
joao.victor <joao.victor@superfestval.com.br>
2022-06-03 15:58:27 -0300

    Componente App

--- navegacao/src/index.js
+++ navegacao/src/index.js
@@ -1,12 +1,11 @@
-// import './index.css'
-import {createRoot} from 'react-dom/client'
+import './views/App.css'
 import React from 'react'
-// import App from './App'
+import {createRoot} from 'react-dom/client'
+import App from './views/App'
 
 const rootElement = document.getElementById('root')
 const root = createRoot(rootElement)
 
 root.render(
-//    <App />
-    <h1>Hello world</h1>
+   <App/>
 )
