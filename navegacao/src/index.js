c52ddaa47f6c48e63704476329a3ae3d97030b8f
joao.victor <joao.victor@superfestval.com.br>
2022-06-03 15:29:51 -0300

    Subindo o index

---
+++ navegacao/src/index.js
@@ -0,0 +1,12 @@
+// import './index.css'
+import {createRoot} from 'react-dom/client'
+import React from 'react'
+// import App from './App'
+
+const rootElement = document.getElementById('root')
+const root = createRoot(rootElement)
+
+root.render(
+//    <App />
+    <h1>Hello world</h1>
+)
