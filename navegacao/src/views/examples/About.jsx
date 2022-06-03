7df04d583ca0ba9ef9e589b820331033a5f22315
joao.victor <joao.victor@superfestval.com.br>
2022-06-03 15:58:47 -0300

    Componente About

---
+++ navegacao/src/views/examples/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = props => {
+    return (
+        <div className="About">
+            <h1>Sobre</h1>
+            <p>Criado om sucesso ...</p>
+        </div>
+    )
+}
+
+export default About
