43540b9b7952bac3db59c53ad046c7bc9888f0a9
joao.victor <joao.victor@superfestval.com.br>
2022-06-10 13:44:54 -0300

    Navegacao com Route Routes

--- navegacao/src/components/layouts/Content.jsx
+++ navegacao/src/components/layouts/Content.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import "./Content.css"
+import { Routes, Route } from 'react-router-dom'
+
+import About from "../../views/examples/About";
+import Home from "../../views/examples/Home";
 
 const Content = props => {
     return (
         <main className="Content">
-            <h1>
-                Componente de conteudo
-            </h1>
+            <Routes>
+                <Route path='/about' element={<About/>}>
+
+                </Route>
+                <Route path='/' element={<Home/>}>
+
+                </Route>
+            </Routes>
+
         </main>
     )
 }
