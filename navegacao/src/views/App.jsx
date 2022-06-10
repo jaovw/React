af5ef99f2ca575eafe1c71da707931e8e695e59c
joao.victor <joao.victor@superfestval.com.br>
2022-06-10 13:44:33 -0300

    React Router

--- navegacao/src/views/App.jsx
+++ navegacao/src/views/App.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import "./App.css"
+import { BrowserRouter as Router } from 'react-router-dom'
 import Content from "../components/layouts/Content";
 import Menu from "../components/layouts/Menu";
 
 const App = props => {
     return (
         <div className="App">
-            <Menu/>
-            <Content/>
+            <Router>
+                <Menu />
+                <Content />
+            </Router>
+
         </div>
     )
 }
