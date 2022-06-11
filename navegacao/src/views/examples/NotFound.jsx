0a325a89060a8f0b839f81552977dddd81f6fe72
jaovw <joojvw@gmail.com>
2022-06-11 11:55:43 -0300

    Componente 404

---
+++ navegacao/src/views/examples/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = _ => {
+    return (
+        <div>
+            <h1>404</h1>
+            <h2>Pagina nao encontrada ...</h2>
+        </div>
+    )
+}
+
+export default NotFound
