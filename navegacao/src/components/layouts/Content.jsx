9e3f5cc07b13545c0d09f8ee661f7f17b20d91ae
jaovw <joojvw@gmail.com>
2022-06-11 11:57:12 -0300

    Redirecionando todas as rotas nao cadastradas

--- navegacao/src/components/layouts/Content.jsx
+++ navegacao/src/components/layouts/Content.jsx
@@ -5,6 +5,7 @@ import { Routes, Route } from 'react-router-dom'
 import About from "../../views/examples/About";
 import Home from "../../views/examples/Home";
 import Param from "../../views/examples/Param";
+import NotFound from "../../views/examples/NotFound";
 
 const Content = _ => {
     return (
@@ -12,15 +13,23 @@ const Content = _ => {
             <Routes>
 
                 <Route 
-                    path='/about' element={<About/>}>
+                    path='/about' 
+                    element={<About/>}>
                 </Route>
 
                 <Route 
-                    path="/param/:id" element={<Param/>}>
+                    path="/param/:id" 
+                    element={<Param/>}>
                 </Route>
                 
                 <Route 
-                    exact path='/' element={<Home/>}>
+                    exact path='/' 
+                    element={<Home/>}>
+                </Route>
+
+                <Route
+                    path="*" 
+                    element={<NotFound/>}>
                 </Route>
 
             </Routes>
