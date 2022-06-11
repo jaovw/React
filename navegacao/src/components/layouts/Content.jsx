4f99dd34437df25dcc9da7da73d33675c95e643c
jaovw <joojvw@gmail.com>
2022-06-11 11:36:42 -0300

    Exact path

--- navegacao/src/components/layouts/Content.jsx
+++ navegacao/src/components/layouts/Content.jsx
@@ -6,19 +6,23 @@ import About from "../../views/examples/About";
 import Home from "../../views/examples/Home";
 import Param from "../../views/examples/Param";
 
-const Content = props => {
+const Content = _ => {
     return (
         <main className="Content">
             <Routes>
-                <Route path='/about' element={<About/>}>
 
+                <Route 
+                    path='/about' element={<About/>}>
                 </Route>
-                <Route path="/param/:id" element={<Param/>}>
 
+                <Route 
+                    path="/param/:id" element={<Param/>}>
                 </Route>
-                <Route path='/' element={<Home/>}>
+                
+                <Route 
+                    exact path='/' element={<Home/>}>
+                </Route>
 
-                </Route>
             </Routes>
 
         </main>
